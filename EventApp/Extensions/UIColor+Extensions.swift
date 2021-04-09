971bc29386ce6739a2c78a53c4974abd43df7e38
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-09 11:36:52 +0800

    Add UIColor extensions

---
+++ EventApp/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  EventApp
+//
+//  Created by Saski Skye on 4/9/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
