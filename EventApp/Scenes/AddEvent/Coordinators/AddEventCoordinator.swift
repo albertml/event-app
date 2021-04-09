525e2f4a7ffbee1b5d4c532ebd6b9dd1917dc77d
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-09 12:33:49 +0800

    Linking update for AddEventCoordinator

--- EventApp/Scenes/AddEvent/Coordinators/AddEventCoordinator.swift
+++ EventApp/Scenes/AddEvent/Coordinators/AddEventCoordinator.swift
@@ -7,20 +7,35 @@
 
 import UIKit
 
+enum AddEventCoordinatorResult {
+    case dismiss
+}
+
 final class AddEventCoordinator: Coordinator {
     private(set) var childCoordinators: [Coordinator] = []
     private let navigationController: UINavigationController
+    var handleEventListCoordinatorResult: ((EventListCoordinatorResult) -> ())?
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
     func start() {
-        let addEventCoordinator = AddEventCoordinator(navigationController: navigationController)
-        
-        childCoordinators.append(addEventCoordinator)
-        addEventCoordinator.start()
+        let addEventController = AddEventController.instantiate()
+        let addEventViewModel = AddEventViewModel()
+        addEventViewModel.addEventCoordinatorResult = handleAddEventCoordinatorResult()
+        addEventController.viewModel = addEventViewModel
+        self.navigationController.present(addEventController, animated: true)
     }
-    
-    
+}
+
+extension AddEventCoordinator {
+    private func handleAddEventCoordinatorResult() -> ((AddEventCoordinatorResult) -> ()) {
+        return { result in
+            switch result {
+            case .dismiss:
+                self.handleEventListCoordinatorResult?(.dismiss)
+            }
+        }
+    }
 }
