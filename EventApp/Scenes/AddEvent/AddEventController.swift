9a264c80b9648449266de6051eb8b1223970e9e4
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-09 12:33:26 +0800

    Add functionalities for AddEventController link

--- EventApp/Scenes/AddEvent/AddEventController.swift
+++ EventApp/Scenes/AddEvent/AddEventController.swift
@@ -15,21 +15,16 @@ class AddEventController: UIViewController {
         return controller
     }
     
+    var viewModel: AddEventProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        viewModel.dismissAddEventScene()
     }
-    */
 
 }
