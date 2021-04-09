bfd7aa26fc1af5502df204e223ea203fe51752ee
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-09 12:34:11 +0800

    Linking update for EventListCoordinator

--- EventApp/Scenes/EventList/Coordinators/EventListCoordinator.swift
+++ EventApp/Scenes/EventList/Coordinators/EventListCoordinator.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum EventListCoordinatorResult {
+    case dismiss
+}
+
 final class EventListCoordinator: Coordinator {
     private(set) var childCoordinators: [Coordinator] = []
     private let navigationController: UINavigationController
@@ -17,12 +21,40 @@ final class EventListCoordinator: Coordinator {
     
     func start() {
         let eventListController = EventListController.instantiate()
+        let eventListViewModel = EventListViewModel()
+        eventListViewModel.eventListCoordinatorResult = handleEventListViewModelCoordinatorResult()
+        eventListController.viewModel = eventListViewModel
         navigationController.setViewControllers([eventListController], animated: false)
     }
     
     private func presentAddEventCoordinator() {
         let addEventCoordinator = AddEventCoordinator(navigationController: navigationController)
+        addEventCoordinator.handleEventListCoordinatorResult = handleEventListCoordinatorResult()
         childCoordinators.append(addEventCoordinator)
         addEventCoordinator.start()
+    }
+}
+
+extension EventListCoordinator {
+    func handleEventListViewModelCoordinatorResult() -> ((EventListViewModelCoordinatorResult) -> ()) {
+        return { result in
+            switch result {
+            case .addEvent:
+                self.presentAddEventCoordinator()
+            case .viewEvent:
+                debugPrint("View Event")
+            }
+        }
+    }
+}
+
+extension EventListCoordinator {
+    func handleEventListCoordinatorResult() -> ((EventListCoordinatorResult) -> ()) {
+        return { result in
+            switch result {
+            case .dismiss:
+                self.childCoordinators.removeLast()
+            }
+        }
     }
 }
