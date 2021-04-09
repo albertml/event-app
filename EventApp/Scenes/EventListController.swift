7f9143abab45aca93992e5f0143d008b501796eb
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-09 11:12:11 +0800

    add initial EventListViewController

---
+++ EventApp/Scenes/EventListController.swift
@@ -0,0 +1,29 @@
+//
+//  EventListController.swift
+//  EventApp
+//
+//  Created by Saski Skye on 4/9/21.
+//
+
+import UIKit
+
+class EventListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
