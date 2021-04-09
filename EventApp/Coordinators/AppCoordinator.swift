e8bae300e29ebd1f4196dfa1bbd34622d409172d
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-09 11:06:40 +0800

    Link AppCoordinator from scene delegate

--- EventApp/Coordinators/AppCoordinator.swift
+++ EventApp/Coordinators/AppCoordinator.swift
@@ -5,13 +5,26 @@
 //  Created by Saski Skye on 4/9/21.
 //
 
-import Foundation
+import UIKit
 
 protocol Coordinator {
     var childCoordinators: [Coordinator] { get }
     func start()
 }
 
-final class AppCoordinator {
+final class AppCoordinator: Coordinator {
+    private(set) var childCoordinators: [Coordinator] = []
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
     
 }
