57369675bba63f7deaffac08d711b3bc611e5b86
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-11 15:06:22 +0800

    Add UITableViewDataSource and delegate for event list tableview

--- EventApp/Scenes/EventList/EventListController.swift
+++ EventApp/Scenes/EventList/EventListController.swift
@@ -9,16 +9,30 @@ import UIKit
 
 class EventListController: UIViewController {
     
+    @IBOutlet weak var eventsTableView: UITableView!
+    
     var viewModel: EventListProtocol!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavigationViews()
-        debugPrint(viewModel.fetchEvents())
+        registerCells()
+        viewModel.eventListViewModelResult = handleViewModelResult()
+        viewModel.fetchEvents()
     }
     
-    private func setupNavigationViews() {
+    @objc private func onAddEventTapped() {
+        viewModel.showAddEventScene()
+    }
+}
+
+
+// MARK: - Setup
+
+private extension EventListController {
+    
+    func setupNavigationViews() {
         let image = UIImage(systemName: "plus.circle.fill")
         let barButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(onAddEventTapped))
         barButtonItem.tintColor = .primary
@@ -27,7 +41,59 @@ class EventListController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    @objc private func onAddEventTapped() {
-        viewModel.showAddEventScene()
+    func registerCells() {
+        let eventCellNib = UINib(nibName:
+            "EventsListTableViewCell", bundle: nil)
+        eventsTableView.register(eventCellNib, forCellReuseIdentifier: "EventsListTableViewCell")
+    }
+}
+
+
+// MARK: - EventListViewModelResult
+
+extension EventListController {
+    func handleViewModelResult() -> ((EventListViewModelResult) -> ()) {
+        return { [weak self] result in
+            guard let s = self else { return }
+            switch result {
+            case .refresh:
+                s.eventsTableView.reloadData()
+            }
+        }
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension EventListController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        viewModel.events.count
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let eventCell = tableView.dequeueReusableCell(withIdentifier: "EventsListTableViewCell", for: indexPath) as? EventsListTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return eventCell
+    }
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension EventListController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        213
     }
 }
