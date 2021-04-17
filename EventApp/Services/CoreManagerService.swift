47c4f4e0e1a9c7c64306ed38d547357cb8d46266
Albert <alberto.gaudicos@appetiser.com.au>
2021-04-17 21:45:53 +0800

    Add CoreManagerService functionalities

---
+++ EventApp/Services/CoreManagerService.swift
@@ -0,0 +1,35 @@
+//
+//  CoreManagerService.swift
+//  EventApp
+//
+//  Created by Saski Skye on 4/17/21.
+//
+
+import UIKit
+
+protocol CoreDataServiceProtocol {
+    func saveEvent(name: String, date: Date, image: UIImage)
+    func getEvents() -> [Event]
+}
+
+final class CoreDataService: CoreDataServiceProtocol {
+    
+    private let coreDataManager = CoreDateManager()
+    
+    func saveEvent(name: String, date: Date, image: UIImage) {
+        
+        let event = Event(context: coreDataManager.moc)
+        event.setValue(name, forKey: "name")
+        
+        let resizedImage = image.sameAspectRatio(newHeight: 250)
+        let imageData = resizedImage.jpegData(compressionQuality: 0.5)
+        event.setValue(imageData, forKey: "image")
+        event.setValue(date, forKey: "date")
+        
+        coreDataManager.saveObject(object: event)
+    }
+    
+    func getEvents() -> [Event] {
+        return coreDataManager.getObjects()
+    }
+}
